158fec1be3f99155760ee03a6938646bd8d61159
Manish-Kush <manishkush2424@gmail.com>
2019-06-11 23:40:43 +0530

    first commit by developer One

--- PracticeJavaCodes/src/com/java/codes/Demo2.java
+++ PracticeJavaCodes/src/com/java/codes/Demo2.java
@@ -7,9 +7,11 @@ public  class Demo2
 
 	public void test2()
 	{
+		System.out.println("comment given by the developer 1");
 		System.out.println("test1() methosd");
 		
 		HashMap t = new HashMap();
 		t.equals("abc");
+		t.equals("Dev 1 comment");
 	}
 }
